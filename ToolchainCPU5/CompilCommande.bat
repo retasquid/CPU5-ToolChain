d676ccd44ae75f9ba141431984e1ee6be1941188
Noé <159713479+retasquid@users.noreply.github.com>
2026-03-07 09:52:02 +0100

    auto compilation
    add task.json to VS code and ctrl+shift+B to compile the curent file.c

--- ToolchainCPU5/CompilCommande.bat
+++ ToolchainCPU5/CompilCommande.bat
@@ -1,9 +1,17 @@
+@echo off
+set FILENAME=%1
+set FILEPATH=%2
+
+REM Si pas d'argument, utiliser main par défaut
+if "%FILENAME%"=="" set FILENAME=main
+if "%FILEPATH%"=="" set FILEPATH=%CD%\main.c
+
 @cd Ccomp
 @Win_bison -d parser.y
 @Win_flex lexer.l
 @gcc parser.tab.c lex.yy.c -o CcompilerCPU5
-@gcc -E -P ../../main.c -o CompFiles/main.i
-@CcompilerCPU5.exe CompFiles/main.i -o CompFiles/main.asm
+@gcc -E -P "%FILEPATH%" -o CompFiles/%FILENAME%.i
+@CcompilerCPU5.exe CompFiles/%FILENAME%.i -o CompFiles/%FILENAME%.asm
 @cd ../
-@python3 ASMcomp/ASMcompilerCPU5.py Ccomp/CompFiles/main.asm ASMcomp/CompFiles/prog.py
+@python3 ASMcomp/ASMcompilerCPU5.py Ccomp/CompFiles/%FILENAME%.asm ASMcomp/CompFiles/prog.py
 @python3 Flasher3.1/PC_flasher/programmer.py ASMcomp/CompFiles/prog.py
